48b0fb21e3d3338abef7b3179d2434fb709e47aa
Ahmed-Reda-elshahawy <ahmed92.5elshahawy@gmail.com>
2025-08-26 20:58:07 +0300

    Add project files.

---
+++ Design-Patterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using Design_Patterns.Singleton;
+
+namespace Design_Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = Logger.Instance;
+
+            // Set the minimum log level
+            logger.CurrentLogLevel = LogLevel.Warning;
+
+            logger.Log("This is an info message", LogLevel.Info);     // Not shown
+            logger.Log("This is a warning", LogLevel.Warning);       // Shown
+            logger.Log("This is an error!", LogLevel.Error);         // Shown
+        }
+    }
+}
---
+++ Design-Patterns/Singleton/Logger.cs
@@ -0,0 +1,31 @@
+﻿namespace Design_Patterns.Singleton
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> _lazyInstance =
+            new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance => _lazyInstance.Value;
+
+        // Current log level (default: Info)
+        public LogLevel CurrentLogLevel { get; set; } = LogLevel.Info;
+
+        // Private constructor so no one can create new instances
+        private Logger() { }
+
+        public void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            if (level >= CurrentLogLevel) // only log if it meets the threshold
+            {
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] [{level}] {message}");
+            }
+        }
+    }
+}
